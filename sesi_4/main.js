844beaf870c26ffd350ef8061ee44ffb016d8c89
Bahrudi-tech <rudyrud93@gmail.com>
2026-08-14 13:00:59 +0700

    commit 2 sesi 4

---
+++ sesi_4/main.js
@@ -0,0 +1,6 @@
+const { kalkulator } = require("./kalkulator.js");
+
+console.log(kalkulator(20, 10, "+"));
+console.log(kalkulator(75, 15, "-"));
+console.log(kalkulator(8, 5, "*"));
+console.log(kalkulator(100, 4, "/"));
